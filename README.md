6b60c38664b2914866ea73b1af9e447d0f2d1659
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2016-07-13 12:27:53 +0200

    CSV cleaning. I can run now Clojure scripts from the Eclipse "Run ..." menu

--- README.md
+++ README.md
@@ -3,11 +3,19 @@
 ### Leiningen
 
 lein grafter run demografia.core/convert-persons-data ./data/example-data.csv output.csv
+
 lein grafter run demografia.core/convert-persons-data ./data/example-data.csv output.xlsx
+
 lein grafter run demografia.core/convert-persons-data-to-graph ./data/example-data.csv ./output/output.ttl
+
+lein grafter run demografia.core/convert-ataun-to-data ./data/ataun2014.csv ./output/output-ataun.csv
 
 ### REPL
 
 => (demografia.core/convert-persons-data-to-graph "./data/example-data.csv")
+
+=> (demografia.core/convert-ataun-to-data "./data/ataun2014.csv")
+
+
 
 
--- src/demografia/core.clj
+++ src/demografia/core.clj
@@ -1,55 +1,30 @@
 (ns demografia.core
-    (:require
-     [grafter.tabular :refer [_ add-column add-columns apply-columns
+  (:require [grafter.tabular :refer [_ add-column add-columns apply-columns
                               build-lookup-table column-names columns
                               derive-column drop-rows graph-fn grep make-dataset
                               mapc melt move-first-row-to-header read-dataset
                               read-datasets rows swap swap take-rows
-                              test-dataset test-dataset]]
-     [grafter.rdf :refer [s]]
-     [grafter.rdf.protocols :refer [->Quad]]
-     [grafter.rdf.templater :refer [graph]]
-     [grafter.pipeline :refer [declare-pipeline]]
-     [grafter.vocabularies.rdf :refer :all]
-     [grafter.vocabularies.foaf :refer :all]
-     [demografia.prefix :refer [base-id base-graph base-vocab base-data]]
-     [demografia.transform :refer [->integer]]))
-
-;; Declare our graph template which will destructure each row and
-;; convert it into an RDF graph.  This will be the final step in our
-;; pipeline definition.
-
-(def make-graph
-  (graph-fn [{:keys [name sex age person-uri gender]}]
-            (graph (base-graph "example")
-                   [person-uri
-                    [rdf:a foaf:Person]
-                    [foaf:gender sex]
-                    [foaf:age age]
-                    [foaf:name (s name)]])))
+                              test-dataset test-dataset ]]
+            [grafter.pipeline :refer [declare-pipeline]]
+    )
+  )
 
 
-;; Declare a pipe so the plugin can find and run it.  It's just a
-;; function from Datasetable -> Dataset.
-(defn convert-persons-data
-  "Pipeline to convert tabular persons data into a different tabular format."
+
+(defn convert-ataun-to-data
+  "Pipeline to convert tabular Ataun demografia data into a different tabular format."
   [data-file]
   (-> (read-dataset data-file)
       (drop-rows 1)
-      (make-dataset [:name :sex :age])
-      (derive-column :person-uri [:name] base-id)
-      (mapc {:age ->integer
-             :sex {"f" (s "female")
-                   "m" (s "male")}})))
+      )
+  )
 
-(declare-pipeline convert-persons-data [Dataset -> Dataset]
-                  {data-file "A data file"})
 
-(defn convert-persons-data-to-graph
-  "Pipeline to convert the tabular persons data sheet into graph data."
-  [dataset]
-  (-> dataset convert-persons-data make-graph))
+(declare-pipeline convert-ataun-to-data [Dataset -> Dataset]
+                  {data-file "Demografia ataun"})
 
-(declare-pipeline convert-persons-data-to-graph [Dataset -> (Seq Statement)]
-                  {dataset "The data file to convert into a graph."})
+;(read-dataset "./data/example-data.csv")
+;(println (convert-ataun-to-data "./data/ataun2014.csv"))
+;(print (convert-ataun-to-data "./data/example-data.csv"))
+(print (convert-ataun-to-data "./data/ataun-coma-result-2014.csv"))
 
